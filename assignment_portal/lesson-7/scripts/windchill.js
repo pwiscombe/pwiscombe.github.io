286edc29946cf92d7b88083e5695302da9a0dac2
Peter Wiscombe <pwiscombe@gmail.com>
2018-06-06 17:54:46 -0400

    chill

--- assignment_portal/lesson-7/scripts/windchill.js
+++ assignment_portal/lesson-7/scripts/windchill.js
@@ -1,13 +1,13 @@
-var high = 50;
-var low = 37;
+var high = 90;
+var low = 66;
+var avg = (high + low) / 2;
 var speed = 10;
 
-function windchill(temp, wind){
-    var chill=Math.round(35.74 + (0.6215 * temp)- (35.75*Math.pow(wind,0.16)) +(0.4275 * temp * Math.pow(wind,0.16)));
-    return chill;
+function calcChill(temp, wind) {
+    var windchill = Math.round(35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16)));
+    return windchill;
 }
-var highChill = windchill(high,speed);
-var lowChill = windchill(low,speed);
 
-document.getElementById("high_feel").innerHTML = "Feels like: "+highChill+ "&deg;F";
-document.getElementById("low_feel").innerHTML = "Feels like: "+lowChill+ "&deg;F";
+var chill = calcChill(avg, speed);
+
+document.getElementById("chilltemp").innerHTML = "Chill:<br>"+ chill + "&deg;F";
