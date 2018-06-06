5e64c3015ad79a3df831b1ce5b7d597df3a359b4
Peter Wiscombe <pwiscombe@gmail.com>
2018-06-06 17:57:38 -0400

    wind speed

--- assignment_portal/lesson-7/scripts/windchill.js
+++ assignment_portal/lesson-7/scripts/windchill.js
@@ -1,7 +1,7 @@
 var high = 90;
 var low = 66;
 var avg = (high + low) / 2;
-var speed = 10;
+var speed = 5;
 
 function calcChill(temp, wind) {
     var windchill = Math.round(35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16)));
