96fc2d9994178d1d43a613b9a3b51fa033a9face
Peter Wiscombe <pwiscombe@gmail.com>
2018-06-23 23:07:18 -0400

    removed sample

--- assignment_portal/lesson-9/scripts/weatherapi.js
+++ assignment_portal/lesson-9/scripts/weatherapi.js
@@ -24,20 +24,3 @@ weatherObject.onload = function () {
     document.getElementById('lowTemp').innerHTML= Math.round(parseFloat(tempLow));
 
 }
-/*
-var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET', 'http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=130fd29c0ecfd2034fcab509cb06f917&units=imperial', true);
-weatherObject.send();
-weatherObject.onload = function () {
-    var weatherInfo = JSON.parse(weatherObject.responseText);
-    console.log(weatherInfo);
-
-    document.getElementById('city').innerHTML = weatherInfo.name;
-    document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp;
-    document.getElementById('currentWind').innerHTML = weatherInfo.wind.speed;
-
-    var iconcode = weatherInfo.weather["0" ].icon;
-    var icon_path = "http://openweathermap.org/img/w/" +iconcode +".png";
-    document.getElementById('weather_icon').src = icon_path; */
-
- // end of weather function
