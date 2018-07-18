87392c82540ec466a84d45c3321f8a85c89e2574
Peter Wiscombe <pwiscombe@gmail.com>
2018-07-18 19:21:07 -0400

    initial final

---
+++ assignment_portal/the_mountain_spoke/scripts/hamburger.js
@@ -0,0 +1,19 @@
+var urlString, urlArray, pageHREF, menu, i, currentURL;
+urlString = document.location.href;
+urlArray = urlString.split('/');
+pageHREF = urlArray[urlArray.length - 1];
+
+if (pageHREF !== "") {
+    menu = document.querySelectorAll('ul#primaryNav li a');
+    for (i = 0; i < menu.length; i++) {
+        currentURL = (menu[i].getAttribute('href'));
+        menu[i].parentNode.className = '';
+        if (currentURL === pageHREF) {
+            menu[i].parentNode.className = 'active';
+        }
+    }
+}
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
